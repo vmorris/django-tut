8031f3ca291941c73afc1bc1bcb74b8f25e91eb3
Vance <vmorris@us.ibm.com>
2018-06-18 06:57:02 -0500

    working on part 2

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.details, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^
 ]
